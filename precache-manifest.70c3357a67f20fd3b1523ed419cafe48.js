d8dadb2888132b975036ea4953c9273477bc2c01
kholland950 <kevinholland94@gmail.com>
2020-08-29 15:41:47 -0400

    Fixed missing pics

---
+++ precache-manifest.70c3357a67f20fd3b1523ed419cafe48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3341633d6ad8b63117f024c89db1b84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fef976c3afcf8044455",
+    "url": "/static/js/2.d976bee4.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.d976bee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26fc631f86998765f02c",
+    "url": "/static/js/main.d5fed54a.chunk.js"
+  },
+  {
+    "revision": "8a21a9610ca2905bab57",
+    "url": "/static/js/runtime-main.89f24a37.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.490a67d3c495a86163060a41e8f1be27.js"
+  "/precache-manifest.70c3357a67f20fd3b1523ed419cafe48.js"
 );
 
 self.addEventListener('message', (event) => {
